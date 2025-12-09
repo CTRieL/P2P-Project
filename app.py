921fc5f394701116f054a93a549a5475ea12d556
Ariel Josua <arieljo145@gmail.com>
2025-12-09 21:38:11 +0700

    adding verbose mode (log) and enchance UI

--- app.py
+++ app.py
@@ -5,11 +5,20 @@ from p2p.p2p import P2PMessenger
 
 def main():
     if len(sys.argv) < 3:
-        print("invalid command: python app.py <PORT> <USERNAME>")
+        print("invalid command: python app.py <PORT> <USERNAME> [OPTIONS]")
+        print("Options:")
+        print("  -v, --debug    Aktifkan mode log raw")
         return
     
+    verbose_mode = False
+    if len(sys.argv) > 3 and (sys.argv[3] == '-v' or sys.argv[3] == '--debug'):
+        verbose_mode = True
+    
+    if verbose_mode:
+        print("[!] Mode Verbose aktif!")
+        
     print("=== P2P Messenger ===")
-    app = P2PMessenger(int(sys.argv[1]), sys.argv[2])
+    app = P2PMessenger(int(sys.argv[1]), sys.argv[2], verbose=verbose_mode)
     app.start()
 
 if __name__ == "__main__":
--- p2p/p2p.py
+++ p2p/p2p.py
@@ -7,13 +7,15 @@ from cryptography.fernet import Fernet
 from p2p.discovery import DiscoveryService
 from security import SecurityManager
 from config import BUFFER_SIZE
+from utils import Colors
 
 class P2PMessenger:
-    def __init__(self, port, username) :
+    def __init__(self, port, username, verbose=True) :
         self.port = port
         self.username = username
         self.security = SecurityManager()
         self.discovery = DiscoveryService(port, username)
+        self.verbose = verbose
 
         #state koneksi
         self.connected_peer_socket = None
@@ -21,7 +23,7 @@ class P2PMessenger:
         self.peer_name = None
 
     def start(self):
-        print(f"[*] Memulai node P2P pada port {self.port}...")
+        print(f"{Colors.HEADER}[*] Memulai node P2P pada port {self.port}...{Colors.ENDC}")
         self.discovery.start()
 
         server_thread = threading.Thread(target=self.tcp_listener, daemon=True)
@@ -49,6 +51,7 @@ class P2PMessenger:
             while True:
                 data = client_sock.recv(BUFFER_SIZE)
                 if not data: break
+                self.log_debug("IN", data)
 
                 packet = json.loads(data.decode())
 
@@ -57,6 +60,7 @@ class P2PMessenger:
                         "type" : "PUBKEY_RESP",
                         "pubkey" : self.security.get_public_key_pem(),
                     }).encode()
+                    self.log_debug("OUT", response)
                     client_sock.send(response)
                     
                 elif packet['type'] == 'HANDSHAKE_FIN':
@@ -66,23 +70,23 @@ class P2PMessenger:
                     self.peer_name = packet['username']
                     self.connected_peer_socket = client_sock
                     
-                    print(f"\n[*] Terhubung dengan {packet['username']}")
+                    print(f"\n{Colors.GREEN}[*] Terhubung dengan {packet['username']}{Colors.ENDC}")
                     print(f"{self.username}: ", end="", flush=True)
                     
                 elif packet['type'] == 'MESSAGE':
                     encrypted_msg = packet['content']
                     msg = self.peer_fernet.decrypt(encrypted_msg.encode()).decode()
 
-                    print(f"\n[{packet['sender']}]: {msg}")
+                    print(f"\n{Colors.BLUE}[{packet['sender']}]: {msg}{Colors.ENDC}")
                     print(f"{self.username}: ", end="", flush=True)
                 
                 elif packet['type'] == 'DISCONNECT':
-                    print(f"\n[!] {self.peer_name} memutus koneksi")
+                    print(f"\n{Colors.FAIL}[!] {self.peer_name} memutus koneksi{Colors.ENDC}")
                     self.reset_connection()
                     break
                     
         except Exception as e:
-            print(f"\n[!] Error Koneksi: {e}")
+            print(f"\n{Colors.FAIL}[!] Error Koneksi: {e}{Colors.ENDC}")
             print(f"{self.username}: ", end="", flush=True)
         finally:
             self.reset_connection()
@@ -94,33 +98,40 @@ class P2PMessenger:
             sock.connect((target_ip, int(target_port)))
 
             #request public key
-            sock.send(json.dumps({
+            req_data = json.dumps({
                 "type" : "PUBKEY_REQ", 
                 "username" : self.username,
-            }).encode())
+            }).encode()
+            self.log_debug("OUT", req_data)
+            sock.send(req_data)
+            
             
             #terima public key teman
             resp = sock.recv(BUFFER_SIZE)
+            self.log_debug("IN", resp)
+            
             resp_json = json.loads(resp.decode())
             peer_pub_pem = resp_json['pubkey']
             
             #enkripsi session key pakai public key teman dan dikirim
             encrypted_sess_key = self.security.encrypt_with_peer_public(self.security.session_key, peer_pub_pem)
-            sock.send(json.dumps({
+            final_handshake = json.dumps({
                 "type" : "HANDSHAKE_FIN",
                 "session_key" : encrypted_sess_key.hex(),
                 "username": self.username,
-            }).encode())
-            
+            }).encode()
+            self.log_debug("OUT", final_handshake)            
+            sock.send(final_handshake)
+
             self.peer_fernet = self.security.fernet #pakai key sendiri
             self.connected_peer_socket = sock
-            print(f"[*] Terhubung ke {target_ip}:{target_port}")
+            print(f"{Colors.GREEN}[*] Terhubung dengan {target_ip}:{target_port}{Colors.ENDC}")
 
             threading.Thread(target=self.handle_client, args=(sock,), daemon=True).start()
 
         except Exception as e:
-            print(f"[!] Gagal connect: {e}")
-            print(f"{self.username}: ", end="", flush=True)
+            print(f"{Colors.FAIL}[!] Gagal connect: {e}{Colors.ENDC}")
+            # print(f"{self.username}: ", end="", flush=True)
             
     def disconnect(self):
         """Mengirim sinyal putus dan reset state"""
@@ -130,10 +141,11 @@ class P2PMessenger:
                     "type" : "DISCONNECT",
                     "sender" : self.username,
                     })
+                self.log_debug("OUT", msg)
                 self.connected_peer_socket.send(msg.encode())
                 self.connected_peer_socket.close()
             except: pass
-            print("[*] Memutus koneksi.")
+            print(f"{Colors.FAIL}[*] Memutus koneksi.{Colors.ENDC}")
             print(f"{self.username}: ", end="", flush=True)
         self.reset_connection()
 
@@ -142,6 +154,18 @@ class P2PMessenger:
         self.peer_fernet = None
         self.peer_name = None
         # print("\n[*] Mode Standby (Listening). Ketik 'list' untuk cari teman.")
+
+    def log_debug(self, direction, data):
+        """"Menampilkan data RAW JSON bila verbose aktif"""
+        if self.verbose:
+            prefix = "[>>> SEND]" if direction == "OUT" else "[RECV <<<]"
+            print(f"{prefix}:")
+            try: 
+                parsed = json.loads(data) if isinstance(data, str) else json.loads(data.decode())
+                print(f"{Colors.WARNING}{json.dumps(parsed, indent=2)}{Colors.ENDC}")
+            except:
+                print(f"{Colors.WARNING}{data}{Colors.ENDC}")
+            print("-" * 30)
     
     def input_loop(self):
         print("\n-- Command --")
@@ -160,21 +184,21 @@ class P2PMessenger:
                 #/list
                 if cmd.lower() == 'list':
                     peers = self.discovery.get_active_peers()
-                    print("\n--- User Online ---")
+                    print(f"{Colors.HEADER}--- User Online ---{Colors.ENDC}")
                     if not peers: print("Belum ada user lain terdeteksi.")
                     for p in peers: print(p)
-                    print("-------------------")
+                    print(f"{Colors.HEADER}-------------------{Colors.ENDC}")
 
                 #/connect <IP> <PORT>
                 elif cmd.startswith('connect'):
                     if self.connected_peer_socket:
-                        print("[!] Anda harus disconnect dulu sebelum ganti teman!")
+                        print(f"{Colors.FAIL}[!] Anda harus disconnect dulu sebelum ganti teman!{Colors.ENDC}")
                         continue
                     try:
                         _, ip, port = cmd.split()
                         self.connect_to_peer(ip, port)
                     except:
-                        print("[!] Format salah. Gunakan: connect <IP> <PORT>")
+                        print("[!] Format salah. Gunakan: /connect <IP> <PORT>")
 
                 # /disconnect
                 elif cmd.lower() == 'disconnect':
@@ -190,15 +214,16 @@ class P2PMessenger:
             elif self.connected_peer_socket:
                 if self.peer_fernet:
                     encrypted_content = self.peer_fernet.encrypt(cmd.encode()).decode()
-                    packet = json.dumps({
+                    msg = json.dumps({
                         "type": "MESSAGE",
                         "sender": self.username,
                         "content": encrypted_content
-                    })
+                    }).encode()
                     try:
-                        self.connected_peer_socket.send(packet.encode())
+                        self.log_debug("OUT", msg)
+                        self.connected_peer_socket.send(msg)
                     except:
                         print("[!] Gagal kirim. Koneksi putus.")
                         self.reset_connection()
             else:
-                print("[!] Anda belum terhubung. Ketik 'list' lalu 'connect <IP> <PORT>'")
+                print("[!] Anda belum terhubung. Ketik '/list' lalu '/connect <IP> <PORT>'")
